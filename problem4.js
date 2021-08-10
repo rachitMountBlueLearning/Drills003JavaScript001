f2ae3d23c9886aba5a051b24030166c55ae87ab5
Rachit Chaudhary <rachit.chaudhary.17@mountblue.tech>
2021-08-10 20:42:32 +0530

    Solve Problem 4

---
+++ problem4.js
@@ -0,0 +1,15 @@
+/*
+    ==== Problem #4 ====
+    The accounting team needs all the years from every car on the lot.
+    Execute a function that will return an array from the dealer data containing only the car years and log the result in the console as it was returned.
+*/
+
+module.exports = {
+    problem4: function (inventory) {
+        let yearList = [];
+        for(let itemIndex = 0; itemIndex < inventory.length; itemIndex++) {
+            yearList.push(inventory[itemIndex].car_year);
+        }
+        return  yearList;    
+    }
+}
---
+++ test/testProblem4.js
@@ -0,0 +1,6 @@
+const inventory = require('../inventory').inventory; //Import inventory data.
+const problem4 = require('../problem4').problem4; //Import solution function.
+
+
+let result = problem4(inventory);  // Calls solution function "problem4()".
+console.log(result); // Logs the result.
