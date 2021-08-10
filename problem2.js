321b81558174aa5731dd436b31cd9d0281236139
Rachit Chaudhary <rachit.chaudhary.17@mountblue.tech>
2021-08-10 20:41:58 +0530

    Solve Problem 2

---
+++ problem2.js
@@ -0,0 +1,19 @@
+/*
+    ==== Problem #2 ====
+    The dealer needs the information on the last car in their inventory.
+    Execute a function to find what the make and model of the last car in the inventory is? 
+    Log the make and model into the console in the format of: 
+    "Last car is a *car make goes here* *car model goes here*"
+*/
+
+module.exports = {
+    problem2: function (inventory) {
+        maxIDIndex = 0;
+        for(let itemIndex = 0; itemIndex < inventory.length; itemIndex++) {
+            if(inventory[itemIndex].id > inventory[maxIDIndex].id){
+                maxIDIndex = itemIndex;
+            }
+        }
+        return  `Last car is a ${inventory[maxIDIndex].car_make} ${inventory[maxIDIndex].car_model}`;    
+    }
+}
---
+++ test/testProblem2.js
@@ -0,0 +1,6 @@
+const inventory = require('../inventory').inventory; //Import inventory data.
+const problem2 = require('../problem2').problem2; //Import solution function.
+
+
+let result = problem2(inventory);  // Calls solution function "problem2()".
+console.log(result); // Logs the result.
