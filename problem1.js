781e5313665fbf3f78acc133c305a3440897fa92
Rachit Chaudhary <rachit.chaudhary.17@mountblue.tech>
2021-08-10 20:41:26 +0530

    Solve Problem 1

---
+++ problem1.js
@@ -0,0 +1,17 @@
+/*
+    ==== Problem #1 ====
+    The dealer can't recall the information for a car with an id of 33 on his lot.
+    Help the dealer find out which car has an id of 33 by calling a function that will return the data for that car.
+    Then log the car's year, make, and model in the console log in the format of: 
+    "Car 33 is a *car year goes here* *car make goes here* *car model goes here*"
+*/
+
+module.exports = {
+    problem1: function (inventory, targetID = 33) {
+        for(let itemIndex = 0; itemIndex < inventory.length; itemIndex++) {
+            if(inventory[itemIndex].id === targetID){
+                return  `Car ${inventory[itemIndex].id} is a ${inventory[itemIndex].car_year} ${inventory[itemIndex].car_make} ${inventory[itemIndex].car_model}`;
+            }
+        }    
+    }
+} 
---
+++ test/testProblem1.js
@@ -0,0 +1,6 @@
+const inventory = require('../inventory').inventory; //Import inventory data.
+const problem1 = require('../problem1').problem1; //Import solution function.
+
+
+let result = problem1(inventory);  // Calls solution function "problem1()".
+console.log(result); // Logs the result.
